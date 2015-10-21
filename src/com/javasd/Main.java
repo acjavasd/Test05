ff0d6c5cb16dd533fa27aa646adb8ff482f4f9b5
Almir Campos <ac@javasd.com>
2015-10-21 12:33:48 -0700

    First commit

---
+++ src/com/javasd/Main.java
@@ -0,0 +1,10 @@
+package com.javasd;
+
+/**
+ * Created by almircampos on 10/21/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test 05");
+    }
+}
